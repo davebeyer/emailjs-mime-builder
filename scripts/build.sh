f358d474a4095669ef33927b71f3232003ca67c6
Dave Beyer <dave.beyer@gmail.com>
2020-08-29 02:44:33 -0700

    Don't automatically go git commit when building

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,9 @@
 
 rm -rf $PWD/dist
 babel src --out-dir dist --ignore '**/*-unit.js' --source-maps inline
-git reset
-git add $PWD/dist
-git commit -m 'Updating dist files' -n
+
+# DB: we do git operations intentionally
+#
+# git reset
+# git add $PWD/dist
+# git commit -m 'Updating dist files' -n
